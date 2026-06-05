fbabfdc42c9e82b25f26956a227e491963d7a737
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-06-05 20:08:17 +0530

    resolve google login error

--- server/dist/controllers/user.js
+++ server/dist/controllers/user.js
@@ -12,6 +12,7 @@ export const loginUser = TryCatch(async (req, res) => {
     }
     const googleRes = await oauth2client.getToken(code);
     oauth2client.setCredentials(googleRes.tokens);
+    console.log("TOKENS => ", googleRes.tokens);
     const userRes = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`);
     const { email, name, picture } = userRes.data;
     let user = await User.findOne({ email });
