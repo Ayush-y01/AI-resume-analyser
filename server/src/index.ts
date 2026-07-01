8ede57e7cb37dd79a1efa525f5b608badd9be254
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-07-01 20:22:02 +0530

    backend setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,17 @@ import dotenv from "dotenv"
 import connnectDB from "./config/db.js";
 import userRoutes from './routes/user.js'
 import aiRoutes from "./routes/ai.js"
+import paymentsRoutes from "./routes/payment.js"
 import cors from "cors"
+import Razorpay from "razorpay"
 
 dotenv.config()
+
+export const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY!,
+    key_secret:process.env.RAZORPAY_SECRET!,
+
+})
 
 await connnectDB();
 
@@ -17,6 +25,8 @@ app.use(express.urlencoded({extended:true, limit: "10mb"}))
 
 app.use("/api/user", userRoutes)
 app.use("/api/ai", aiRoutes)
+app.use("/api/payments", paymentsRoutes)
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
