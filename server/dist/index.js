83590be1798168db616bc2cb2b43ac2c1e2a8f46
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-08-23 20:14:48 +0530

    some updates

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,8 +3,14 @@ import dotenv from "dotenv";
 import connnectDB from "./config/db.js";
 import userRoutes from './routes/user.js';
 import aiRoutes from "./routes/ai.js";
+import paymentsRoutes from "./routes/payment.js";
 import cors from "cors";
+import Razorpay from "razorpay";
 dotenv.config();
+export const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET,
+});
 await connnectDB();
 const app = express();
 app.use(cors());
@@ -12,6 +18,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/user", userRoutes);
 app.use("/api/ai", aiRoutes);
+app.use("/api/payments", paymentsRoutes);
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
